e20524938cbd18d8cc60fab6720c75f875971a20
Daniel Echeverri <echeverri@gmail.com>
2024-02-07 11:42:19 -0500

    First commit, nav bar with basic state functionalities seted

---
+++ PairExpenses.Client/Entities/User.cs
@@ -0,0 +1,16 @@
+namespace PairExpenses.Client;
+using System.Collections.Generic;
+
+
+public class User
+{
+    public int Id { get; set; }
+    public required string Name { get; set; }
+
+    public long Total { get; set; } = 0;
+    public long TotalDebt { get; set; } = 0;
+
+    public List<Debt>? Debts {get; set;}
+
+    public List<Payment>? Payments {get; set;}
+}
